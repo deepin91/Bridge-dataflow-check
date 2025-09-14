34a4bb7617503d427a28f989e2ec169a30ae1710
deepin <deepin3809@gmail.com>
2025-09-14 20:55:41 +0900

    //

--- src/main/java/bridge/configuration/SwaggerConfiguration.java
+++ src/main/java/bridge/configuration/SwaggerConfiguration.java
@@ -1,35 +1,35 @@
-package bridge.configuration;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-//import springfox.documentation.builders.ApiInfoBuilder;
-//import springfox.documentation.builders.PathSelectors;
-//import springfox.documentation.builders.RequestHandlerSelectors;
-//import springfox.documentation.service.ApiInfo;
-//import springfox.documentation.spi.DocumentationType;
-//import springfox.documentation.spring.web.plugins.Docket;
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-
-@Configuration
-public class SwaggerConfiguration {
-
-	@Bean
-	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2)
-				.apiInfo(apiInfo())
-				.select()
-				.apis(RequestHandlerSelectors.basePackage("bridge"))
-				.paths(PathSelectors.any())
-				.build();
-	}
-	
-	private ApiInfo apiInfo() {
-		return new ApiInfoBuilder()
-				.title("Spring Boot Open API with Swagger")
-				.description("Bridge REST API")
-				.version("1.0.0")
-				.build();
-	}
-}
+//package bridge.configuration;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+////import springfox.documentation.builders.ApiInfoBuilder;
+////import springfox.documentation.builders.PathSelectors;
+////import springfox.documentation.builders.RequestHandlerSelectors;
+////import springfox.documentation.service.ApiInfo;
+////import springfox.documentation.spi.DocumentationType;
+////import springfox.documentation.spring.web.plugins.Docket;
+//import io.swagger.v3.oas.models.OpenAPI;
+//import io.swagger.v3.oas.models.info.Info;
+//
+//@Configuration
+//public class SwaggerConfiguration {
+//
+//	@Bean
+//	public Docket api() {
+//		return new Docket(DocumentationType.SWAGGER_2)
+//				.apiInfo(apiInfo())
+//				.select()
+//				.apis(RequestHandlerSelectors.basePackage("bridge"))
+//				.paths(PathSelectors.any())
+//				.build();
+//	}
+//	
+//	private ApiInfo apiInfo() {
+//		return new ApiInfoBuilder()
+//				.title("Spring Boot Open API with Swagger")
+//				.description("Bridge REST API")
+//				.version("1.0.0")
+//				.build();
+//	}
+//}
